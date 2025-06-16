a8c1adc8ee53f930a83a562f13886c95b1b15eb6
hovanthanh1988 <luckylucke.thanh@gmail.com>
2025-06-16 07:15:20 +0700

    update run_all.sh file to run all testcase

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Exit on first error
 set -e
 
 echo "Running pytest..."
-pytest testcases -k test_login_success --alluredir=reports/allure-results
+pytest testcases --alluredir=reports/allure-results
 
 echo "Generating Allure report..."
 allure generate reports/allure-results -o reports/allure-report --clean
